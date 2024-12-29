71bf8c11b11a6cdb4ba5ffef900b71fba7aa4760
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-12-30 00:56:26 +0700

    feat: add homepage

---
+++ src/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>the famous cat</h2>
+  <h6>this week's most famous cat:</h6>
+  <div class="home-cat image">
+    <img src="./assets/cats/tree-cat.png" alt="">
+  </div>
+  <p>can your cat placed 1st on weekly famous cat? find out here!</p>
+  <router-link to="/all-cats">see all cats!</router-link>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
