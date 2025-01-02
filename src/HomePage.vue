040c8646c02ab1501039929e5604888f3a017d42
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-03 00:00:09 +0700

    feat: add link to cat form

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
-  <h2>the famous cat</h2>
+  <h1>the famous cat</h1>
   <h6>this week's most famous cat:</h6>
   <div class="home-cat image">
     <img src="./assets/cats/tree-cat.png" alt="">
   </div>
-  <p>can your cat placed 1st on weekly famous cat? find out here!</p>
+  <p>can your cat placed 1st on weekly famous cat? find out <router-link to="/add-new">here</router-link>!</p>
   <div class="cta">
     <router-link to="/all-cats">see all cats!</router-link>
   </div>
