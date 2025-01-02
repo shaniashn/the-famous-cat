e6943a5fbf8d5fb9d1cac0b1051c15e467718057
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-02 23:59:17 +0700

    feat: add dummy cat data

--- src/AllCats.vue
+++ src/AllCats.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="dashboard">
-  <div class="welcome">
-    <h1>welcome, username!</h1>
-  <h2>your cats</h2>
-  <div class="cats-gallery">
-    <div class="card">
-      <img src="./assets/cats/peace-cat.png" alt="">
-      <h3>tabby</h3>
-      <p><span>0</span> likes</p>
+    <div class="welcome">
+      <h1>welcome, username!</h1>
+      <h2>your cats</h2>
+      <div class="cats-gallery">
+        <div class="card">
+          <img src="./assets/cats/peace-cat.png" alt="">
+          <h3>tabby</h3>
+          <p><span>0</span> likes</p>
+        </div>
+        <div class="card">
+          <img src="./assets/cats/rawr-cat.png" alt="">
+          <h3>roare</h3>
+          <p><span>0</span> likes</p>
+        </div>
+        <div class="card">
+          <img src="./assets/blankcat.svg" alt="">
+          <h3>+ add more</h3>
+        </div>
+      </div>
     </div>
-    <div class="card">
-      <img src="./assets/cats/rawr-cat.png" alt="">
-      <h3>roare</h3>
-      <p><span>0</span> likes</p>
-    </div>
-    <div class="card">
-      <img src="./assets/blankcat.svg" alt="">
-      <h3>+ add more</h3>
-    </div>
-  </div>
-  </div>
-  <PreviewPage />
+    <PreviewPage />
   </div>
 </template>
 
