65a79e922019939bd3edf297e304372a49d6a1ef
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-03 01:13:56 +0700

    fix: add row gender on forms

--- src/CatForm.vue
+++ src/CatForm.vue
@@ -1,18 +1,18 @@
 <template>
-  <h1>add new cat</h1>
   <div class="form-cat">
-    <form action="">
+    <h1>add new cat</h1>
+    <div class="form"><form action="">
       <label for="name">name</label>
       <input type="text" id="name">
       <label for="age">age</label>
       <input type="text" id="age">
       <label for="breed">breed</label>
       <input type="text" id="breed">
+      <label for="gender">gender</label>
+      <input type="text" id="gender">
       <label for="hobby">hobby</label>
       <input type="text" id="hobby">
-      <label for="hates">hates</label>
-      <input type="text" id="hates">
-    </form>
+    </form></div>
     <div class="image">
       <img @click="logFile()" src="./assets/blankcat.svg">
       <label for="input-file">upload picture</label>
@@ -42,5 +42,11 @@ export default {
 <style>
 form {
   flex-direction: column;
+}
+
+label,
+input {
+  display: block;
+  text-align: start;
 }
 </style>
