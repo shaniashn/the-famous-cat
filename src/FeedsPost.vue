5f624a0e2e4830c9abc6830ca191a029a3f3fca2
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-09 23:58:37 +0700

    feat: update feeds layout

--- src/FeedsPost.vue
+++ src/FeedsPost.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1>feeds</h1>
-  <div class="container">
-    <input type="text" placeholder="title">
+  <div class="feeds">
+    <h1>feeds</h1>
+    <div class="container">
+      <input type="text" placeholder="title">
+      <textarea name="" id="" rows="10">text</textarea>
+      <button>post</button>
+    </div>
   </div>
 </template>
 
@@ -15,3 +19,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.feeds{
+  display: grid;
+}
+
+.container {
+  height: auto;
+  max-width: 100%;
+  width: 90%;
+  display: grid;
+  justify-self: center;
+  border: 2px solid black;
+  padding: 15px;
+  gap: 10px;
+}
+</style>
