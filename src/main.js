cea04095d22438e353f681968996a5ad9f3a6ab8
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-14 20:34:58 +0700

    feat: add icon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).use(router).mount('#app')
+library.add({ faTrophy });
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
