33450650145b65aebea3de8c90fa9c24a012cfd5
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-15 23:59:11 +0700

    style(ui): fix homepage layout

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>the famous cat</h1>
-  <h6>this week's most famous cat:</h6>
-  <div class="home-cat image">
+  
+  <div class="home-cat">
+    <h6>this week's most famous cat:</h6>
     <img src="./assets/cats/tree-cat.png" alt="">
   </div>
   <p>can your cat placed 1st on weekly famous cat? add your cat and find out <router-link to="/add-new">here</router-link>!</p>
@@ -19,5 +20,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h6 {
+  font-size: 1rem;
+  margin: 0;
+  margin-bottom: 10px;
+}
+
+p {
+  margin: 3% 0;
+}
+
+.home-cat {
+  margin: 5% 0;
+}
+
+.home-cat img {
+  max-width: 25%;
+}
 </style>
