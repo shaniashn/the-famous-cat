c0b6927dd7e9e4f5c22745ee5292509fd3bd5798
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-17 00:53:17 +0700

    feat: add menu component

---
+++ src/MenuComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-wrapper">
+    <img src="./assets/cats/rawr-cat.png" alt="profile-image">
+    <h3>shane</h3>
+    <span>@shanshine</span>
+    <!-- <span><router-link>my cats</router-link></span>
+  <span><router-link>ranks</router-link></span>
+  <span><router-link>media</router-link></span> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuComponent',
+}
+</script>
+
+<style scoped>
+.menu-wrapper {
+  width: 50%;
+  margin: 4% 0;
+  border-right: 0.5px solid #898989;
+  height: 100%;
+}
+
+.menu-wrapper img {
+  max-width: 30%;
+}
+</style>
