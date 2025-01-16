602d1823ad7b10aa8e6d5f10c499f1e7a49f519c
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-17 00:55:15 +0700

    feat: import fontawesome icon heart

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers'
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTrophy, faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 
-library.add({ faTrophy });
+library.add({ faTrophy, fasHeart, farHeart });
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
