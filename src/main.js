ffc4587c173379379412f4012b8aa23b444722f8
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-01-31 23:22:23 +0700

    feat: add bar menu

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './routers'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faTrophy, faHeart as fasHeart, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faTrophy, faHeart as fasHeart, faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 
-library.add({ faTrophy, fasHeart, farHeart, faXmark });
+library.add({ faTrophy, fasHeart, farHeart, faXmark, faBars });
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app');
