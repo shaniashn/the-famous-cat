a4d2814b1e84a2bb24c00c2d2425cb01d76637b3
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-03-23 11:55:12 +0700

    refactor(ui): remove old styles
    moved to _global.scss

--- src/App.vue
+++ src/App.vue
@@ -8,35 +8,14 @@ export default {
 }
 </script>
 
-<style>
-/* colors
-898989 gray line
-*/
+<style lang="scss">
+@import './assets/scss/main.scss';
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
-}
-
-body {
-  margin: 0;
-  vertical-align: baseline;
-}
-
-ul {
-  margin: 0;
-  padding: 0;
-}
-
-li {
-  list-style: none;
-}
-
-p {
-  margin: 0;
+  color: $font-color;
 }
 </style>
